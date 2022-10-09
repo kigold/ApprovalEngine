788c4e024ca2d6d049f9e6ad55c4fbe400e95058
Kingsley <kingsleybox@yahoo.com>
2022-10-09 16:55:25 +0100

    refactor: rename project, add sample api

---
+++ SampleApp.Api/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok(new[] { "Red", "Green", "Blue" });
+        }
+    }
+}
--- SampleApp.Api/Program.cs
+++ SampleApp.Api/Program.cs
@@ -14,12 +14,10 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
