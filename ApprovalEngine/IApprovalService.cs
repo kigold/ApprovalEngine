c31fb9160d27fb70f0c255fa4b2aef3559573217
Kingsley <kingsleybox@yahoo.com>
2022-10-11 17:19:04 +0100

    feat: add approval engine interface

---
+++ ApprovalEngine/IApprovalService.cs
@@ -0,0 +1,32 @@
+﻿using ApprovalEngine.Models;
+
+namespace ApprovalEngine
+{
+    public interface IApprovalService
+    {
+        //Delegates
+        event Action<bool, long> OnApproval;
+        event Action<bool, long> OnDecline;
+        event Action<long> OnReject;
+        event Action<long> OnReturn;
+
+        //Action
+        Task<ResultModel<bool>> CreateApprovalRequest(CreateApprovalRequest model);
+        Task<ResultModel<bool>> ApproveRequest(ApprovalModel model);
+        Task<ResultModel<bool>> DeclineRequest(ApprovalModel model);
+        Task<ResultModel<bool>> RejectRequest(ApprovalModel model);
+        Task<ResultModel<bool>> ReturnRequest(ApprovalModel model); //Return back to the user for input
+        Task<ResultModel<bool>> UpdateRequestToPending(long approvalRequestId); //Update request back to pending after user input
+
+        //Gets
+        Task<ResultModel<ApprovalRequestResponse>> GetRequest(long approvalRequestId);
+        Task<ResultModel<PagedList<ApprovalRequestResponse>>> GetRequests(GetApprovalsRequest request);
+        Task<ResultModel<PagedList<ApprovalRequestResponse>>> GetRequestsByPermission(GetApprovalsRequestByPermission request);
+        Task<ResultModel<List<ApprovalHistoryResponse>>> GetRequestsHistory(long approvalRequestId);
+
+        //Admin
+        Task<ResultModel<List<ApprovalStageResponse>>> GetApprovalStages(GetApprovalStageRequest model);
+        Task<ResultModel<bool>> CreateRequestStages(CreateApprovalStage model);
+        Task<ResultModel<bool>> DeleteRequestStages(DeleteApprovalStages model);
+    }
+}
---
+++ ApprovalEngine/Models/ResultModel.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApprovalEngine.Models
+{
+    public class ResultModel<T>
+    {
+        public ResultModel() { }
+        public ResultModel(T data, string message = "")
+        {
+            Data = data;
+            Message = message;
+        }
+        public ResultModel(string errorMessage)
+        {
+            AddError(errorMessage);
+        }
+
+        public ResultModel(List<string> errorMessage)
+        {
+            errorMessage.ForEach(x => AddError(x));
+        }
+
+        public List<ValidationResult> ValidationErrors { get; set; } = new List<ValidationResult>();
+
+        public List<string> ErrorMessages
+        {
+            get
+            {
+                return ValidationErrors.Select(c => c.ErrorMessage).ToList();
+            }
+        }
+
+        public string Message { get; set; }
+
+        public T Data { get; set; } = default;
+        public int TotalCount { get; set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                var validatioResult = ValidationErrors.FirstOrDefault(r => r.MemberNames.FirstOrDefault() == columnName);
+                return validatioResult == null ? string.Empty : validatioResult.ErrorMessage;
+            }
+        }
+
+        public bool HasError
+        {
+            get
+            {
+                if (ValidationErrors.Count > 0)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        public void AddError(string error)
+        {
+            ValidationErrors.Add(new ValidationResult(error));
+        }
+
+        public void AddError(ValidationResult validationResult)
+        {
+            ValidationErrors.Add(validationResult);
+        }
+
+        public void AddError(IEnumerable<ValidationResult> validationResults)
+        {
+            ValidationErrors.AddRange(validationResults);
+        }
+    }
+}
